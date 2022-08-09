6123fafbd0969e4bcdd4e901ef9598cdf0f9c0bb
Manas Vegi <manasvegi@gmail.com>
2022-08-09 13:24:57 +0400

    Add working skeleton

---
+++ user-service/model/repository.js
@@ -0,0 +1,18 @@
+import UserModel from './user-model.js';
+import 'dotenv/config'
+
+//Set up mongoose connection
+import mongoose from 'mongoose';
+
+
+let mongoDB = process.env.DB_CLOUD_URI;
+
+mongoose.connect(mongoDB, { useNewUrlParser: true , useUnifiedTopology: true});
+
+let db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
+export async function createUser(params) { 
+  return new UserModel(params)
+}
+

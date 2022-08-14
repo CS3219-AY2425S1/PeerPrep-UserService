129ca84a305f97f87beea06aecb24dab4ca42da5
Rishi5154 <rishi5154@gmail.com>
2022-08-14 13:19:39 +0800

    Update DB config in repo layer

--- user-service/model/repository.js
+++ user-service/model/repository.js
@@ -5,7 +5,7 @@ import 'dotenv/config'
 import mongoose from 'mongoose';
 
 
-let mongoDB = process.env.DB_CLOUD_URI;
+let mongoDB = process.env.DB_CLOUD_URI || process.env.DB_LOCAL_URI;
 
 mongoose.connect(mongoDB, { useNewUrlParser: true , useUnifiedTopology: true});
 
